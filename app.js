ecb2f0615b0cfbd0960a77c1da1debe6cf070157
Kat <144715725+katkowska@users.noreply.github.com>
2024-02-07 15:33:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -27,29 +27,34 @@ const app = express();
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ConnectionString } = require('mongodb-connection-string-url'); // Add this import
+
 const dbUrl = process.env.DB_URL;
 
-const client = new MongoClient(dbUrl, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+try {
+    const connectionString = new ConnectionString(dbUrl);
+    console.log('Parsed Connection String:', connectionString);
+} catch (error) {
+    console.error('Error Parsing Connection String:', error);
+}
+
+const client = new MongoClient(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 }
+
 run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
